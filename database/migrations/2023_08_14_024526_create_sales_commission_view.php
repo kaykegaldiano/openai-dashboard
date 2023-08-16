b5079933afac32fadb57d8b33386d1898e70851a
Kayke Galdiano <kaykegaldiano@gmail.com>
2023-08-16 00:04:03 -0300

    fixing join in the sales_commission_view migration

--- database/migrations/2023_08_14_024526_create_sales_commission_view.php
+++ database/migrations/2023_08_14_024526_create_sales_commission_view.php
@@ -18,7 +18,7 @@ return new class extends Migration
             ->join('companies as cp', 'cp.id', '=', 'sl.company_id')
             ->join('addresses as ad', 'ad.id', '=', 'cl.address_id')
             ->join('users as us', 'us.id', '=', 'sl.user_id')
-            ->join('users as uc', 'us.id', '=', 'cl.user_id')
+            ->join('users as uc', 'uc.id', '=', 'cl.user_id')
             ->selectRaw('
                 cp.name as company,
                 us.name as seller,
