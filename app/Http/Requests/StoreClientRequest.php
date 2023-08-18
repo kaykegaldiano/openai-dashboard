66c70aae36da318c3080ab951a9d0071230137ad
Kayke Galdiano <kaykegaldiano@gmail.com>
2023-08-18 00:44:41 -0300

    update and store client requests

--- app/Http/Requests/StoreClientRequest.php
+++ app/Http/Requests/StoreClientRequest.php
@@ -11,7 +11,7 @@ class StoreClientRequest extends FormRequest
      */
     public function authorize(): bool
     {
-        return false;
+        return true;
     }
 
     /**
@@ -22,7 +22,9 @@ class StoreClientRequest extends FormRequest
     public function rules(): array
     {
         return [
-            //
+            'name' => 'required|min:3',
+            'email' => 'email|required|unique:users',
+            'address_id' => 'required'
         ];
     }
 }
